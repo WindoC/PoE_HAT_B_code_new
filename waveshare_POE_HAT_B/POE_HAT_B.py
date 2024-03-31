e36b131635e8b30de36406627f4c01a3f6a38eb7
Antonio Cheong <windo.ac@gmail.com>
2024-03-31 17:32:05 +0800

    remove unused code / enhance not fan status not init issue.

--- waveshare_POE_HAT_B/POE_HAT_B.py
+++ waveshare_POE_HAT_B/POE_HAT_B.py
@@ -10,16 +10,10 @@ class POE_HAT_B:
         self.fan = FAN()
         self.led = LED()
         self.hostname = socket.gethostname()
-        # self.fan.FAN_ON()
-        self.FAN_MODE = None
         self.temp = None
-        
-    def GET_IP(self):
-        s=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-        s.connect(('8.8.8.8',80))
-        ip=s.getsockname()[0]
-        s.close()
-        return ip
+        # default turn fan on
+        self.FAN_MODE = True
+        self.fan.FAN_ON()
         
     def GET_Temp(self):
         with open('/sys/class/thermal/thermal_zone0/temp', 'rt') as f:
@@ -37,21 +31,17 @@ class POE_HAT_B:
             changed = True
 
         # set fan
-        if(temp>=(FAN_TEMP+safevalue)):
-            if(self.FAN_MODE==False):
+        if temp>=(FAN_TEMP+safevalue):
+            if self.FAN_MODE==False:
+                changed = True
                 self.FAN_MODE=True
-                changed = True
-                self.fan.FAN_ON()
-                print("Debug: FAN On")
-                os.system("logger POE_HAT_B fan on")
+                self.FAN_ON()
 
-        elif(temp<(FAN_TEMP-safevalue)):
-            if(self.FAN_MODE==True):
-                self.FAN_MODE=False
+        elif temp<(FAN_TEMP-safevalue):
+            if self.FAN_MODE==True:
                 changed = True
-                self.fan.FAN_OFF()
-                print("Debug: FAN Off")
-                os.system("logger POE_HAT_B fan off")
+                self.FAN_MODE=False
+                self.FAN_OFF()
         
         # led display
         if changed:
@@ -59,5 +49,12 @@ class POE_HAT_B:
             print(f"Debug: display H:{self.hostname} Temp:{int(temp)} FAN:{'ON' if self.FAN_MODE else 'OFF'}")
 
     # to support old code
+    def FAN_ON(self):
+        self.fan.FAN_ON()
+        print("Debug: FAN On")
+        os.system("logger POE_HAT_B fan on")
+
     def FAN_OFF(self):
         self.fan.FAN_OFF()
+        print("Debug: FAN Off")
+        os.system("logger POE_HAT_B fan off")
