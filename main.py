37763f213424b5c42a669f425f4a5a83016a2372
Antonio Cheong <windo.ac@gmail.com>
2024-10-02 11:18:39 +0800

    add logic allow to change setting by os env

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 import time
-
 import logging
+import os
 
 from waveshare_POE_HAT_B.POE_HAT_B import POE_HAT_B
 
 logging.basicConfig(level=logging.INFO)
+
+def getenv_float(env_name:str,default_if_null:float) -> float:
+    temp = os.getenv(env_name)
+    try:
+        return float(temp)
+    except:
+        return float(default_if_null)
+        
+POE_HAT_TEMPERATURE = getenv_float('POE_HAT_TEMPERATURE', 50.0)
+POE_HAT_DELTA = getenv_float('POE_HAT_DELTA', 10.0)
+print(f"Debug: POE_HAT_TEMPERATURE = {POE_HAT_TEMPERATURE} , POE_HAT_DELTA = {POE_HAT_DELTA}")
 
 def main():
     
@@ -12,7 +23,7 @@ def main():
 
     try:  
         while(1):
-            POE.POE_HAT_Display(50.0,10.0)
+            POE.POE_HAT_Display(POE_HAT_TEMPERATURE,POE_HAT_DELTA)
             time.sleep(10)
             
     except KeyboardInterrupt:    
